c2ee700b178570a158feb5e858221a12a4f663d5
Jon Aldrete <jon.aldrete9@gmail.com>
2022-02-10 13:22:54 -0800

    Aldrete_hw3

---
+++ js/p2.js
@@ -0,0 +1,29 @@
+let DayEntered = prompt("Enter a day of the week:")
+let FollowingDay
+switch (DayEntered) {
+   case "sun":
+      FollowingDay = "mon";
+      break;
+   case "mon":
+      FollowingDay = "tue";
+      break;
+   case "tue":
+      FollowingDay = "wed";
+      break;
+   case "wed":
+      FollowingDay = "thu";
+      break;
+   case "thu":
+      FollowingDay = "fri";
+      break;
+   case "fri":
+      FollowingDay = "sat";
+      break;
+    case "sat":
+     FollowingDay = "sun";
+     break;
+      defualt:
+      console.log("Not a valid day type");
+}
+console.log(`You entered:${DayEntered}`);
+console.log(`The next day is ${FollowingDay}`);
