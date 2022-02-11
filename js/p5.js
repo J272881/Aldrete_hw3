69bf70317ddad79168e51d70cb044f7d180511f5
Jon Aldrete <jon.aldrete9@gmail.com>
2022-02-10 18:38:44 -0800

    Aldrete_Hw3 (3.0)

--- js/p5.js
+++
@@ -1,7 +0,0 @@
-let number = prompt('Enter a number:');
-var multiple;
-let answer;
-for (multiple = 1; multiple <= 10; multiple++) {
-    answer = number * multiple
-    console.log(number + "*" + multiple + "=" + answer)
-}
